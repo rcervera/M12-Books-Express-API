842fdb16b6052e7a2a1adc3994dae03bd0c0b4ff
Ramon <rcerver4@xtec.cat>
2023-03-09 13:48:36 +0100

    petits canvis

--- app.js
+++ app.js
@@ -4,43 +4,40 @@ var dotenv = require('dotenv');
 var session = require('express-session');
 var mongoose = require('mongoose');
 
-var cors = require('cors')
-
-
-var publisherRouter = require('./routes/publisherRouter');
-
-
-
 var app = express();
+
+// Client i servidor s'executen en diferents ports
+// Apareixen problemes amb Cross-Origin Resource Sharing
+// El paquet 'cors' habilita cors amb possibilitat de passar diferents opcions
+var cors = require('cors')
 app.use(cors())
 
+
+// Connexió BD MONGO
 dotenv.config();
-
-
-var mongoDB = process.env.MONGODB_URI;
-    
+var mongoDB = process.env.MONGODB_URI;    
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-   
 
-//app.use(express.urlencoded({ extended: true }));
+const db = mongoose.connection;
+db.on("error", function () {
+  console.log("Connection error!!");
+  process.exit(1);
+});
+db.once("open", function () {
+  console.log("Connected successfully");
+}); 
 
+// Format intercanvi de dades JSON
 app.use(express.json());
 
-
-app.use(express.static(path.join(__dirname + '/public')));
-
-
+// Ruta de prova
 app.get('/',function(req, res) {  
      res.send('API VERSION')
 });
 
-
+// Rutes CRUD per gestionar taula bàsica
+var publisherRouter = require('./routes/publisherRouter');
 app.use('/publisher', publisherRouter);
-
-
 
 
 module.exports = app;
